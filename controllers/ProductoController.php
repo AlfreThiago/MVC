9ac09b1b3ff1076142b9e1e2dd3d46295ccc28ec
AlfreThiago <hiranarizaga@gmail.com>
2025-06-09 23:22:24 -0300

    Update ProductoController.php

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -2,7 +2,7 @@
 require_once("models/Producto.php");
 
 class ProductoController {
-    public function listar(){
+    public function index(){
         $producto= new Producto();
         $resultados=$producto->listar();
         include("views/producto/listar.php");
