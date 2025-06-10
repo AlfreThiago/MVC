f92af97b21e8345197de5a7f37cc79ec874d0738
AlfreThiago <hiranarizaga@gmail.com>
2025-06-09 23:15:59 -0300

    EDIT

--- Models/Producto.php
+++ Models/Producto.php
@@ -10,5 +10,13 @@ class Producto {
         $sql="SELECT*FROM productos";
         return $this->db->query($sql);
     }
+    public function actualizar($id,$nombre,$precio){
+        $sql="UPDATE productos SET nombre='$nombre', precio='$precio' WHERE id=$id";
+        return $this->db->query($sql);
+    }
+    public function buscarPorId($id){
+        $sql="SELECT*FROM productos WHERE id=$id";
+        return $this->query($sql)->fetch_assoc();
+    }
 }
 ?>
