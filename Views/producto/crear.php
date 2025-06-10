aa4c11ff3868565a3d407f94608df4f09fd2d3d7
AlfreThiago <hiranarizaga@gmail.com>
2025-06-10 17:59:42 -0300

    Update crear.php

--- Views/producto/crear.php
+++ Views/producto/crear.php
@@ -1,4 +1,4 @@
-<form method="post" action="index
+<form method="post" action="index.php?accion=crear">
     Nombre: <input type="text" name="nombre">
     Precio: <input type="number" step="0.01" name="precio">
 </form>
