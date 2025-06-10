e7fcfe9d42de305f567b2818b454bd8daba188e8
AlfreThiago <hiranarizaga@gmail.com>
2025-06-09 23:28:31 -0300

    asa

---
+++ Views/producto/crear.php
@@ -0,0 +1,4 @@
+<form method="post" action="index
+    Nombre: <input type="text" name="nombre">
+    Precio: <input type="number" step="0.01" name="precio">
+</form>
--- Views/producto/listar.php
+++ Views/producto/listar.php
@@ -1,4 +1,6 @@
 <h2>Lista de Producto</h2>
 <?php foreach ($resultados as $p): ?>
-    <p><?=$p['nombre']?>-$<?=$p['precio']?></p>
+    <p><?=$p['nombre']?>-$<?=$p['precio']?>
+<a href="index.php?accion=editar&id=<?=$p['id']?>">
+</p>
     <?php endforeach;?>
