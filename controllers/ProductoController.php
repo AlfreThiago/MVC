5e5502af862f897365aaae7a0f269094918e6104
AlfreThiago <hiranarizaga@gmail.com>
2025-06-09 23:20:03 -0300

    s

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -2,7 +2,7 @@
 require_once("models/Producto.php");
 
 class ProductoController {
-    public function index(){
+    public function listar(){
         $producto= new Producto();
         $resultados=$producto->listar();
         include("views/producto/listar.php");
