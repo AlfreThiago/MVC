97c515672c0f0b91e2b06689cb3ed9703236fcb3
AlfreThiago <hiranarizaga@gmail.com>
2025-06-17 22:51:16 -0300

    Update listar.php

--- Views/usuario/listar.php
+++ Views/usuario/listar.php
@@ -1,6 +1,6 @@
 <h2>Lista de Usuarios</h2>
 <?php foreach ($resultados as $u): ?>
-    <p><?=$p['nombre']?>-$<?=$u['contraseña']?>
+    <p><?=$u['nombre']?>-$<?=$u['contraseña']?>
 <a href="index.php?accion=editar&id=<?=$u['id']?>">
 </p>
     <?php endforeach;?>
